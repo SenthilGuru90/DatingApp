ad1832d826d62b1cc51c20d81fedd7221b514b5e
senthilmurugan gurusamy <senthilmurugan.guru@gmail.com>
2020-04-01 12:00:45 +0530

    additional changes

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
-import { MemberCardComponent } from "./members/member-card/member-card.component";
+import { MemberCardComponent } from './members/member-card/member-card.component';
 import { ListComponent } from './list/list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
